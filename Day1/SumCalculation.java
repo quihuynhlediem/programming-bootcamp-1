2a46b72850973eea8d13395d7bd146fbd7b1a8cd
Qui Huynh Le Diem <huynhlediemqui.vn@gmail.com>
2023-10-27 20:16:22 +0700

    Rename class demo

--- Day1/SumCalculation.java
+++ Day1/SumCalculation.java
@@ -2,7 +2,7 @@ package Day1;
 
 import java.util.Scanner;
 
-public class demo {
+public class SumCalculation {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int firstNumber = sc.nextInt();

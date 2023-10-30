af0e2b2f1e3e57346031e58b2a1687289a76449f
Qui Huynh Le Diem <huynhlediemqui.vn@gmail.com>
2023-10-30 13:19:01 +0700

    Add trial file to repo

---
+++ ScannerTrial.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+public class ScannerTrial {
+    public static void main(String[] args){
+       String input = "Good morning everyone!";
+       Scanner scnr = new Scanner(input);
+       String token1;
+       token1 = scnr.nextLine();
+       System.out.print(token1);
+       scnr.close();
+    }
+}

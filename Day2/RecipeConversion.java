091095ec1e51394cb6d1e569e4e29a066a3ebb9c
Qui Huynh Le Diem <huynhlediemqui.vn@gmail.com>
2023-11-05 10:49:18 +0700

    Done code for Week 1

--- Day2/RecipeConversion.java
+++ Day2/RecipeConversion.java
@@ -5,36 +5,26 @@ import java.util.Scanner;
 public class RecipeConversion {
     public static void main(String[] args){
         Scanner scnr = new Scanner(System.in);
-
-        // Constants
-        final double CONVERSION_RATE = 250.0;  // 1 Australian cup = 250 grams
-        final double PRICE_PER_CUP = 2.64;     // Cost per cup
-
-        // Step 1
         System.out.println("Please input the name of your ingredient:");
-        String ingredientName = scnr.nextLine();
-
+        String ingredient = scnr.nextLine();
         System.out.println("Please input the measurement in grams:");
-        double measurementGrams = Double.parseDouble(scnr.nextLine());
-
+        double measurement = scnr.nextDouble();
         System.out.println("Please input the scale factor:");
-        double scaleFactor = Double.parseDouble(scnr.nextLine());
+        double scaleFactor = scnr.nextDouble();
+        System.out.println();
 
-        // Convert grams to cups and scale it
-        double measurementCups = (measurementGrams / CONVERSION_RATE) * scaleFactor;
+        //Step 1
+        final int GRAMS_PER_CUP = 250;
+        double cupsNum = measurement / GRAMS_PER_CUP * scaleFactor;
 
-        System.out.println(); // Added explicit newline for separation
-        System.out.printf("%-15s%-15s%-15s%s%s\n", "Ingredient", "Grams", "Scale Factor", "Cups", "Test");
-        System.out.printf("%-15s%-15.0f%-15.1f%.2f\n", ingredientName, measurementGrams, scaleFactor, measurementCups);
+        System.out.println("Ingredient     " + "Grams          " + "Scale Factor   " + "Cups");
+        System.out.printf("%-15s%-15.0f%-15.1f%.2f", ingredient, measurement, scaleFactor, cupsNum);
+        System.out.println();
+        System.out.println();
 
-        // Step 2
-        double totalCost = measurementCups * PRICE_PER_CUP;
-
-        // Rounding up
-        totalCost = Math.ceil(totalCost);
-
-        // Displaying the cost directly without if-else check
-        System.out.printf("\nThe cost of %.0f cups at $%.2f per cup is $%,.0f", measurementCups, PRICE_PER_CUP, totalCost);
-        scnr.close();
+        //Step 2
+        final double PRICE_PER_CUP = 2.64;
+        double totalCost = cupsNum * PRICE_PER_CUP;
+        System.out.printf("The cost of %.0f cups at $2.64 per cup is $%,.0f", cupsNum, totalCost);
     }
 }
